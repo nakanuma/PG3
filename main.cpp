da4f78da279db4a6af8c8857ad4f899c829db6a1
nakanuma <k023g0121@g.neec.ac.jp>
2024-09-30 16:42:23 +0900

    途中

--- main.cpp
+++ main.cpp
@@ -1,5 +1,43 @@
+#include <Windows.h>
+#include <random>
 #include <stdio.h>
 
+// 1~6の値をランダムで返す関数
+uint32_t GenerateRandomNumber() {
+	// 乱数生成器の初期化
+	std::random_device rd;
+	std::mt19937 rng(rd());
+
+	std::uniform_int_distribution<uint32_t> distNumber(1, 6);
+
+	return distNumber(rng);
+}
+
 int main() {
+	// 関数ポインタの定義
+	uint32_t (*randomNumberFunc)() = GenerateRandomNumber;
+
+	// プレイヤーの入力を受け取る
+	char input;
+	printf("半(奇数)なら0、丁(偶数)なら1を入力 : ");
+	scanf_s("%c", &input);
+
+	// 3秒待機
+	Sleep(3000);
+
+	// 数字を生成
+	uint32_t randomNumber = randomNumberFunc();
+	printf("出た数字 : %d\n", randomNumber);
+
+	// 2秒待機
+	Sleep(2000);
+
+	// 入力の検証
+	if ((randomNumber % 2 == 1 && input == '0') || (randomNumber % 2 == 0 && input == '1')) {
+		printf("正解\n");
+	} else {
+		printf("不正解\n");
+	}
+
 	return 0;
 }
