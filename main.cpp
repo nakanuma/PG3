b6c5a82d3e3e9c873ba1a9b262caa3a97cca8b01
nakanuma <k023g0121@g.neec.ac.jp>
2024-10-07 11:30:26 +0900

    確認課題

--- main.cpp
+++ main.cpp
@@ -13,6 +13,30 @@ uint32_t GenerateRandomNumber() {
 	return distNumber(rng);
 }
 
+// 指定秒待機する関数
+void WaitSeconds(uint32_t seconds) { 
+	Sleep(seconds * 1000); 
+}
+
+// 入力検証を行う関数
+void ValidateInput(uint32_t randomNumber, char input) {
+	// 入力の検証
+	if ((randomNumber % 2 == 1 && input == '0') || (randomNumber % 2 == 0 && input == '1')) {
+		printf("正解\n");
+	} else {
+		printf("不正解\n");
+	}
+}
+
+// 待機してから入力検証を行うコールバック関数
+void WaitAndValidate(uint32_t randomNumber, char input, void (*waitCallback)(uint32_t), void (*validateCallback)(uint32_t, char)) {
+	// コールバックで3秒待機
+	waitCallback(3);
+
+	// コールバックで入力検証
+	validateCallback(randomNumber, input);
+}
+
 int main() {
 	// 関数ポインタの定義
 	uint32_t (*randomNumberFunc)() = GenerateRandomNumber;
@@ -26,15 +50,8 @@ int main() {
 	uint32_t randomNumber = randomNumberFunc();
 	printf("出た数字 : %d\n", randomNumber);
 
-	// 3秒待機
-	Sleep(3000);
-
-	// 入力の検証
-	if ((randomNumber % 2 == 1 && input == '0') || (randomNumber % 2 == 0 && input == '1')) {
-		printf("正解\n");
-	} else {
-		printf("不正解\n");
-	}
+	// 待機してから入力の検証をコールバックで実行
+	WaitAndValidate(randomNumber, input, WaitSeconds, ValidateInput);
 
 	return 0;
 }
