c330e169d77c3b13dccec2d9543d006e6d27b50d
nakanuma <k023g0121@g.neec.ac.jp>
2024-10-28 11:18:16 +0900

    確認課題

--- main.cpp
+++ main.cpp
@@ -7,44 +7,27 @@
 #include <cmath>
 #include <iostream>
 
-#include "Enemy.h"
+#include "Compare.h"
 
 int main() {
-	// 敵のインスタンスを作成
-	std::unique_ptr<Enemy> enemy_;
-	enemy_ = std::make_unique<Enemy>();
+	// 6つのパターンのインスタンスを作成して、小さい方の値を表示
+	Compare<int, int> intIns(11, 7);
+	std::cout << "Min(int, int)" << intIns.Min() << std::endl;
 
-	// 前回フェーズの保存
-	Enemy::Phase previousPhase = static_cast<Enemy::Phase>(-1);
+	Compare<float, float> floatIns(5.2f, 13.5f);
+	std::cout << "Min(float, float)" << floatIns.Min() << std::endl;
 
-	// ゲームループ
-	while (true) {
-		enemy_->Update();
+	Compare<double, double> doubleIns(42.8, 27.1);
+	std::cout << "Min(double, double)" << doubleIns.Min() << std::endl;
 
-		// 現在のフェーズを取得
-		Enemy::Phase currentPhase = enemy_->GetCurrentPhase();
+	Compare<int, float> intFloatIns(32, 18.8f);
+	std::cout << "Min(int, float)" << intFloatIns.Min() << std::endl;
 
-		// フェーズ変更が行われた際のみフェーズを表示
-		if (currentPhase != previousPhase) {
-			std::cout << "Current Phase:" << enemy_->GetPhaseName() << std::endl;
-			previousPhase = currentPhase;
-		}
+	Compare<int, double> intDoubleIns(9, 21.4);
+	std::cout << "Min(int, double)" << intDoubleIns.Min() << std::endl;
 
-		// Approachフェーズのときに座標を表示
-		if (currentPhase == Enemy::APPROACH) {
-			const Vector3& pos = enemy_->GetPosition();
-			std::cout << "position.z:" << pos.z << std::endl;
-		}
-
-		// Fireフェーズのときに残弾を表示
-		if (currentPhase == Enemy::FIRE) {
-			const int32_t& leftBullet = enemy_->GetLeftBullet();
-			std::cout << "leftBullet:" << leftBullet << std::endl;
-		}
-
-		// 確認しやすいように長めに遅延
-		std::this_thread::sleep_for(std::chrono::milliseconds(300));
-	}
+	Compare<float, double> floatDoubleIns(89.4f, 56.2);
+	std::cout << "Min(float, double)" << floatDoubleIns.Min() << std::endl;
 
 	return 0;
 }
