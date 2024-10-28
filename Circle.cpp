716230a8e5218a9f26ef67862917e1fa20870627
nakanuma <k023g0121@g.neec.ac.jp>
2024-10-28 12:54:11 +0900

    文字化けの修正

--- Circle.cpp
+++ Circle.cpp
@@ -1,7 +1,10 @@
 #include "Circle.h"
+#define _USE_MATH_DEFINES
+#include <math.h>
 
 void Circle::Size() { 
-	size_ = std::numbers::pi_v<float> * radius_ * radius_; 
+	/*size_ = std::numbers::pi_v<float> * radius_ * radius_; */
+	size_ = static_cast<float>(M_PI) * radius_ * radius_;
 }
 
 void Circle::Draw() { 
