d350ea8c82675a0fa9883546ac58c470a7042e1d
nakanuma <k023g0121@g.neec.ac.jp>
2024-11-12 11:20:10 +0900

    確認課題

--- main.cpp
+++ main.cpp
@@ -1,24 +1,55 @@
 #include <Windows.h>
-#include <stdio.h>
-#include <random>
-#include <functional>
-#include <thread>
 #include <chrono>
 #include <cmath>
+#include <functional>
 #include <iostream>
+#include <list>
+#include <random>
+#include <stdio.h>
+#include <thread>
 #include <vector>
 
-#include "IShape.h"
-#include "Circle.h"
-#include "RectAngle.h"
-
 int main() {
-	std::vector<std::unique_ptr<IShape>> shapes;
-	shapes.push_back(std::make_unique<Circle>(5.0f));
-	shapes.push_back(std::make_unique<RectAngle>(4.0f, 6.0f));
+	std::list<const char*> yamanoteLineStations = {"Tokyo",  "Kanda",     "Akihabara", "Okachimachi",  "Ueno",       "Uguisudani",  "Nippori",   "Tabata",   "Komagome", "Sugamo",
+	                                               "Otsuka", "Ikebukuro", "Mejiro",    "Takadanobaba", "Shin-Okubo", "Shinjuku",    "Yoyogi",    "Harajuku", "Shibuya",  "Ebisu",
+	                                               "Meguro", "Gotanda",   "Osaki",     "Shinagawa",    "Tamachi",    "Hamamatucho", "Shimbashi", "Yurakucho"};
+	                                       
+	// 1970年の駅一覧を表示
+	std::cout << "1970 : " << std::endl;
+	for (auto itr = yamanoteLineStations.begin(); itr != yamanoteLineStations.end(); ++itr) {
+		std::cout << *itr << "\n";
+	}
+	std::cout << "\n" << std::endl;
 
-	for (const auto& shape : shapes) {
-		shape->Draw();
+
+	// 西日暮里駅を追加
+	for (auto itr = yamanoteLineStations.begin(); itr != yamanoteLineStations.end(); ++itr) {
+		if (*itr == "Tabata") {
+			yamanoteLineStations.insert(itr, "Nishi-Nippori");
+		}
+	}
+
+
+	// 2019年の駅一覧を表示
+	std::cout << "2019 : " << std::endl;
+	for (auto itr = yamanoteLineStations.begin(); itr != yamanoteLineStations.end(); ++itr) {
+		std::cout << *itr << "\n";
+	}
+	std::cout << "\n" << std::endl;
+
+
+	// 高輪ゲートウェイ駅を追加
+	for (auto itr = yamanoteLineStations.begin(); itr != yamanoteLineStations.end(); ++itr) {
+		if (*itr == "Tamachi") {
+			yamanoteLineStations.insert(itr, "Takanawa-Gateway");
+		}
+	}
+
+
+	// 2022年の駅一覧を表示
+	std::cout << "2022 : " << std::endl;
+	for (auto itr = yamanoteLineStations.begin(); itr != yamanoteLineStations.end(); ++itr) {
+		std::cout << *itr << "\n";
 	}
 
 	return 0;
