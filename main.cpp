1cd36051a5e03876937a6d38acccce87d9d4f95c
nakanuma <k023g0121@g.neec.ac.jp>
2024-12-02 14:42:44 +0900

    確認課題

--- main.cpp
+++ main.cpp
@@ -1,7 +1,31 @@
 #include <stdio.h>
+#include <iostream>
+#include <mutex>
+#include <condition_variable>
+#include <thread>
+
+std::mutex mtx;
+std::condition_variable cv;
+int current_thread = 1;
+
+void Print(int thread_id, const std::string& message) { 
+	std::unique_lock<std::mutex> lock(mtx);
+	cv.wait(lock, [thread_id]() { return current_thread == thread_id; });
+
+	std::cout << message << std::endl;
+
+	current_thread++;
+	cv.notify_all();
+}
 
 int main() {
-	printf("ハローワールド\n"); 
+	std::thread t1(Print, 1, "thread1");
+	std::thread t2(Print, 2, "thread2");
+	std::thread t3(Print, 3, "thread3");
+
+	t1.join();
+	t2.join();
+	t3.join();
 
 	return 0;
 }
