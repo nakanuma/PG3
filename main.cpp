627b72c4e163fa3b449c8f420dd4c6d629b14804
nakanuma <k023g0121@g.neec.ac.jp>
2025-02-03 11:04:44 +0900

    確認課題

--- main.cpp
+++ main.cpp
@@ -1,31 +1,36 @@
 #include <stdio.h>
 #include <iostream>
-#include <mutex>
-#include <condition_variable>
-#include <thread>
-
-std::mutex mtx;
-std::condition_variable cv;
-int current_thread = 1;
-
-void Print(int thread_id, const std::string& message) { 
-	std::unique_lock<std::mutex> lock(mtx);
-	cv.wait(lock, [thread_id]() { return current_thread == thread_id; });
-
-	std::cout << message << std::endl;
-
-	current_thread++;
-	cv.notify_all();
-}
+#include <chrono>
 
 int main() {
-	std::thread t1(Print, 1, "thread1");
-	std::thread t2(Print, 2, "thread2");
-	std::thread t3(Print, 3, "thread3");
+	// 10万文字の文字列を作成
+	std::string a(100000, 'a');
 
-	t1.join();
-	t2.join();
-	t3.join();
+	///
+	/// コピーにかかる時間を計測
+	/// 
+	std::chrono::high_resolution_clock::time_point start_copy, end_copy;
+	start_copy = std::chrono::high_resolution_clock::now(); // 計測開始時間
+
+	volatile std::string copied_string = a; // 文字列のコピーを行う
+
+	end_copy = std::chrono::high_resolution_clock::now();                                                     // 計測終了時間
+	auto copy_elapsed = std::chrono::duration_cast<std::chrono::microseconds>(end_copy - start_copy).count(); // 処理に要した時間をミリ秒に変換
+
+	std::cout << "コピーに要した時間 : " << copy_elapsed << "μs" << std::endl;
+
+	///
+	///	移動にかかる時間を計測
+	/// 
+	std::chrono::high_resolution_clock::time_point start_move, end_move;
+	start_move = std::chrono::high_resolution_clock::now(); // 計測開始時間
+
+	volatile std::string moved_string = std::move(a); // 文字列の移動を行う
+
+	end_move = std::chrono::high_resolution_clock::now();                                                     // 計測終了時間
+	auto move_elapsed = std::chrono::duration_cast<std::chrono::microseconds>(end_move - start_move).count(); // 処理に要した時間をミリ秒に変換
+
+	std::cout << " 移動に要した時間 : " << move_elapsed << "μs" << std::endl;
 
 	return 0;
 }
